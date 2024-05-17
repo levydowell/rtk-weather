64c6c742290ebed8095cb59d95acf3ae611e4ce0
levydowell <33126924+levydowell@users.noreply.github.com>
2024-05-17 07:08:34 -0600

    Sparklines working for all graphs, and all cities entered are rendered

--- app/components/allData.js
+++ app/components/allData.js
@@ -5,22 +5,41 @@ import { useDispatch, useSelector } from 'react-redux';
 
 export const AllData = () => {
   const weather = useSelector((state) => state.weather.weather);
+  const city = useSelector((state) => state.weather.city);
 
-  let tempData = [];
-  // weather.list.forEach((e) => {
-  // if (weather.list.indexOf(e) % 3 === 0) {
-  //     tempData.push(e);
-  //   }
-  // });
 
   return weather.map((cityWeather) => {
     console.log('cityweather', cityWeather);
     return (
-    <div className='graphs' key={cityWeather.city.name}>
-      <Sparklines data={cityWeather.list.map((datum) => datum.main.temp)} width={20} height={10} margin={1}>
-        <SparklinesLine />
-        <SparklinesReferenceLine type="mean" />
-      </Sparklines>
+    <div className='graph-container' key={cityWeather.city.name}>
+
+      <div className='graphs city-name'>
+        <h4>{cityWeather.city.name}</h4>
+      </div>
+
+      <div className='graphs'>
+        <h2>Temperature</h2>
+        <Sparklines data={cityWeather.list.map((datum) => datum.main.temp)} width={20} height={10} margin={1}>
+          <SparklinesLine color='grey' style={{strokeWidth: 0.5, fill: 'none'}}/>
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+      </div>
+
+      <div className='graphs'>
+        <h2>Pressure</h2>
+        <Sparklines data={cityWeather.list.map((datum) => datum.main.pressure)} width={20} height={10} margin={1}>
+          <SparklinesLine color='red' style={{strokeWidth: 0.5, fill: 'none'}}/>
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+      </div>
+
+      <div className='graphs'>
+        <h2>Humidity</h2>
+        <Sparklines data={cityWeather.list.map((datum) => datum.main.humidity)} width={20} height={10} margin={1}>
+          <SparklinesLine color='blue' style={{strokeWidth: 0.5, fill: 'none'}}/>
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+      </div>
   </div>
     )
   })
--- app/page.js
+++ app/page.js
@@ -7,19 +7,11 @@ import { useState } from 'react';
 import { InputField } from './components/cityInput';
 import { AllData } from './components/allData';
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
 
 
 export default function Home() {
-  const [lineData, setLineData] = useState('');
   
   const weather = useSelector((state) => state.weather.weather);
-  const testLog = weather;
-  console.log('weather', testLog);
-
-  
-
-
   
   if (weather == null) {
     return (
